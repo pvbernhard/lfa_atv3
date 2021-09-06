ab83ec009b01dd5fa68bbf9edd30f814c171877a
Pedro V. Bernhard <pvbernhard@gmail.com>
2021-09-06 13:45:16 -0300

    edit: atualiza AFD.py

--- AFD.py
+++ AFD.py
@@ -1,34 +1,31 @@
 from typing import Optional, Dict, Any
 
 
-class Estado:
-    def __init__(self, nome: str):
-        self.nome = nome
-    
-    def configura(self, estados_possiveis: Dict[str, Any]) -> None:
-        self.estados_possiveis = estados_possiveis
-    
-    def proximo(self, caractere: str) -> Optional[Any]:
-        if caractere in self.estados_possiveis:
-            return self.estados_possiveis[caractere]
-        else:
-            return None
+class Estado:  
+  def configura(self, estados_possiveis: Dict[str, Any]) -> None:
+    self.estados_possiveis = estados_possiveis
+  
+  def proximo(self, caractere: str) -> Optional[Any]:
+    if caractere in self.estados_possiveis:
+      return self.estados_possiveis[caractere]
+    else:
+      return None
 
 class MaquinaEstados:
-    def __init__(self, estado_inicial: Estado, estado_final: Estado):
-        self.estado_inicial = estado_inicial
-        self.estado_final = estado_final
-    
-    def ler(self, palavra: str) -> bool:
-        estado = self.estado_inicial
-        for c in palavra:
-            estado = estado.proximo(c)
-            if estado is None:
-                return False
-        if estado.nome == self.estado_final.nome:
-            return True
-        else:
-            return False
+  def __init__(self, estado_inicial: Estado, estado_final: Estado):
+    self.estado_inicial = estado_inicial
+    self.estado_final = estado_final
+  
+  def ler(self, palavra: str) -> bool:
+    estado = self.estado_inicial
+    for c in palavra:
+      estado = estado.proximo(c)
+      if estado is None:
+        return False
+    if estado == self.estado_final:
+      return True
+    else:
+      return False
 
 
 estado_0 = Estado('estado_0')
@@ -43,12 +40,11 @@ maquina_estados = MaquinaEstados(estado_inicial=estado_0, estado_final=estado_2)
 
 while(True):
     palavra = input('Digite uma palavra:')
+    if palavra == '':
+      break
     resultado = maquina_estados.ler(palavra)
     if resultado:
         print('aceita')
     else:
         print('rejeita')
-    novamente = input('Novamente? [s ou n]')
-    if not novamente in ['s', 'sim', 'S', 'Sim', 'SIM']:
-        break
     print()
